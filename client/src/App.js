502c145bcb299609273f1c7f3227f354db3e6343
Maddy Tan <maddytan7@gmail.com>
2022-07-07 22:33:28 +0000

    includes main upload html

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">
+      <i class="fa-solid fa-microphone-lines"></i>Start Bopping
+      </h4>
     </div>
   );
 }

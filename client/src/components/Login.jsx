4af20b8816e11f9365ec28fdaa86c6c76cd26d18
Adam Yiu <adam_yiu@outlook.com>
2022-07-09 16:14:15 -0400

    Build login auth

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,11 @@
-
+import axios from 'axios';
 
 const Login = () => {
+  axios.get('/')
+    .then((response) => {
+      window.location = "https://accounts.spotify.com"
+    })
+
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>About</h2>

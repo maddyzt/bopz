15dec274f41c464e154867c0bd699c8a4d9ddaf0
Adam Yiu <adam_yiu@outlook.com>
2022-07-11 11:36:15 -0400

    Remove dead code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,63 +4,9 @@ import { Fragment, useEffect, useState } from 'react';
 
 let Login = () => {
 
-
-
-  // let [click, setClick] = useState(false)
-
-  //window.location.href = 'the route'
-
-  // react router (is a wrapper, not dealing with window location object directly)
-
-  //useNavigate(used to go to URLs)
-  // let change = () => {
-  //   console.log('First')
-  //  return axios.get('/login')
-  //   .then((res) => {
-  //     console.log('successful redirect!')
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //   })
-  // }
-
-// With params
-  // const change = () => {
-  //   console.log('First')
-  //  return axios.get('http://localhost:8000/login', {
-  //   params: {
-  //     test: 'test'
-  //   }
-  // })
-  //   // console.log("Second")
-  //   .then((res) => {
-  //     console.log('successful redirect!')
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //   })
-  // }
-
-  // Using State
-  // useEffect(() => {
-  //   axios.get('http://localhost:8000/login')
-  //   .then((res) => {
-  //     console.log('successful redirect!')
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //   })
-  // }, [click])
-
-
-
-
   return (
     <li>
       <a href = 'http://localhost:8080/login'> Login to Spotify </a>
-      {/* <button onClick={() => setClick(true)}> Login to Spotify </button> */}
-      {/* <button onClick={change}> Login to Spotify </button> */}
-
     </li>
 
   );
